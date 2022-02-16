32bca7949e6126e1697396ec80bb1ed1d288fae2
ChondromollikaAhmed <chondromollikaahmed@gmail.com>
2022-02-17 02:31:11 +0600

    Adding Event Viewing Interface

---
+++ src/main/java/com/ca/onyoureve/EditStudioController.java
@@ -0,0 +1,48 @@
+/*
+ * *
+ *  * Created by JehadurRE on 2/17/22, 2:27 AM
+ *  *chondromollikaahmed@gmail.com
+ *  * Copyright (c) 2022 . All rights reserved.
+ *  * Last modified 2/17/22, 2:27 AM
+ *  *Current Path E:/OnYourEVE/src/main/java/com/ca/onyoureve/EditStudioController.java
+ *  *OnYourEVE
+ *
+ */
+
+package com.ca.onyoureve;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+public class EditStudioController {
+    @javafx.fxml.FXML
+    private JFXTextField studio1_contact;
+    @javafx.fxml.FXML
+    private JFXButton save_btn;
+    @javafx.fxml.FXML
+    private JFXTextField studio1_cost;
+    @javafx.fxml.FXML
+    private JFXTextField studio3_name;
+    @javafx.fxml.FXML
+    private JFXButton exit_btn;
+    @javafx.fxml.FXML
+    private JFXTextField studio2_cost;
+    @javafx.fxml.FXML
+    private JFXTextField studio3_cost;
+    @javafx.fxml.FXML
+    private JFXTextField studio2_name;
+    @javafx.fxml.FXML
+    private JFXTextField studio1_name;
+    @javafx.fxml.FXML
+    private JFXTextField studio3_contact;
+    @javafx.fxml.FXML
+    private JFXTextField studio2_contact;
+
+    @javafx.fxml.FXML
+    public void handleSaveButton() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleExitButton() {
+    }
+}

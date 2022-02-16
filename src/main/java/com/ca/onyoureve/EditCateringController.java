06c874d290a60628fa4080c5ebee3c8ad25fe171
ChondromollikaAhmed <chondromollikaahmed@gmail.com>
2022-02-17 02:44:46 +0600

    Adding Edit Catering  Interface

---
+++ src/main/java/com/ca/onyoureve/EditCateringController.java
@@ -0,0 +1,60 @@
+/*
+ * *
+ *  * Created by Chondromollika Ahmed on 2/17/22, 2:41 AM
+ *  *chondromollikaahmed@gmail.com
+ *  * Copyright (c) 2022 . All rights reserved.
+ *  * Last modified 2/17/22, 2:41 AM
+ *  *Current Path E:/OnYourEVE/src/main/java/com/ca/onyoureve/EditCateringController.java
+ *  *OnYourEVE
+ *
+ */
+
+package com.ca.onyoureve;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+public class EditCateringController {
+    @javafx.fxml.FXML
+    private JFXTextField caterer1_price;
+    @javafx.fxml.FXML
+    private JFXButton save_btn;
+    @javafx.fxml.FXML
+    private JFXTextField caterer2_name;
+    @javafx.fxml.FXML
+    private JFXButton exit_btn;
+    @javafx.fxml.FXML
+    private JFXTextField caterer1_specialty;
+    @javafx.fxml.FXML
+    private JFXTextField caterer1_contact;
+    @javafx.fxml.FXML
+    private JFXTextField caterer1_days;
+    @javafx.fxml.FXML
+    private JFXTextField caterer3_name;
+    @javafx.fxml.FXML
+    private JFXTextField caterer2_specialty;
+    @javafx.fxml.FXML
+    private JFXTextField caterer3_specialty;
+    @javafx.fxml.FXML
+    private JFXTextField caterer3_days;
+    @javafx.fxml.FXML
+    private JFXTextField caterer2_contact;
+    @javafx.fxml.FXML
+    private JFXTextField caterer3_contact;
+    @javafx.fxml.FXML
+    private JFXTextField caterer3_price;
+    @javafx.fxml.FXML
+    private JFXTextField caterer2_days;
+    @javafx.fxml.FXML
+    private JFXTextField caterer2_price;
+    @javafx.fxml.FXML
+    private JFXTextField caterer1_name;
+
+    @javafx.fxml.FXML
+    public void handleSaveButton() {
+    }
+
+    @javafx.fxml.FXML
+    public void handleExitButton() {
+    }
+}

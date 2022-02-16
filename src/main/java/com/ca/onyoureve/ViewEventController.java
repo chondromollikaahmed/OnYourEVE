b75cafbfaaa18145d4b66ec25dd8def1e6ee649e
ChondromollikaAhmed <chondromollikaahmed@gmail.com>
2022-02-17 02:08:39 +0600

    Adding Event Viewing Interface

---
+++ src/main/java/com/ca/onyoureve/ViewEventController.java
@@ -0,0 +1,93 @@
+/*
+ * *
+ *  * Created by Chondromollika Ahmed on 2/17/22, 2:01 AM
+ *  *chondromollikaahmed@gmail.com
+ *  * Copyright (c) 2022 . All rights reserved.
+ *  * Last modified 2/17/22, 2:01 AM
+ *  *Current Path E:/OnYourEVE/src/main/java/com/ca/onyoureve/ViewEventController.java
+ *  *OnYourEVE
+ *
+ */
+
+package com.ca.onyoureve;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextField;
+
+public class ViewEventController {
+    @javafx.fxml.FXML
+    private JFXTextField event_price;
+    @javafx.fxml.FXML
+    private JFXTextField venue_name;
+    @javafx.fxml.FXML
+    private JFXTextField venue_loc;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_sugarfree;
+    @javafx.fxml.FXML
+    private JFXButton exit_btn;
+    @javafx.fxml.FXML
+    private JFXCheckBox media_album;
+    @javafx.fxml.FXML
+    private JFXTextField venue_contact;
+    @javafx.fxml.FXML
+    private JFXTextField menu_rice;
+    @javafx.fxml.FXML
+    private JFXTextField venue_cost;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_dryfruit;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_miranda;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_coke;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_sprite;
+    @javafx.fxml.FXML
+    private JFXTextField studio_name;
+    @javafx.fxml.FXML
+    private JFXTextField event_type;
+    @javafx.fxml.FXML
+    private JFXTextField event_date;
+    @javafx.fxml.FXML
+    private JFXTextField venue_addr;
+    @javafx.fxml.FXML
+    private JFXTextField studio_cost;
+    @javafx.fxml.FXML
+    private JFXCheckBox media_video;
+    @javafx.fxml.FXML
+    private JFXTextField menu_cost;
+    @javafx.fxml.FXML
+    private JFXTextField event_guests;
+    @javafx.fxml.FXML
+    private JFXCheckBox media_crane;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_water;
+    @javafx.fxml.FXML
+    private JFXTextField menu_bread;
+    @javafx.fxml.FXML
+    private JFXTextField studio_contact;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_cake;
+    @javafx.fxml.FXML
+    private JFXTextField venue_cap;
+    @javafx.fxml.FXML
+    private JFXCheckBox media_photo;
+    @javafx.fxml.FXML
+    private JFXTextField venue_cat;
+    @javafx.fxml.FXML
+    private JFXTextField menu_protein;
+    @javafx.fxml.FXML
+    private JFXTextField menu_caterer;
+    @javafx.fxml.FXML
+    private JFXCheckBox menu_icecream;
+    @javafx.fxml.FXML
+    private JFXTextField event_name;
+    @javafx.fxml.FXML
+    private JFXCheckBox media_drone;
+    @javafx.fxml.FXML
+    private JFXTextField event_time;
+
+    @javafx.fxml.FXML
+    public void handleExitButton() {
+    }
+}
